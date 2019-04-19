e192570aa390a625dfac5eb7716372b4a2128b82
matheusrcruz <git.dev@outlook.com>
2019-04-19 16:37:26 -0300

    ADD: Pagina de teste chat

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import LoginWrapper from './components/LoginWrapper';
-import BtnBotcamp from './components/BtnBotcamp';
-import LogoBotcamp from './components/LogoBotcamp';
+
+
+import Chat from './pages/Chat';
+import Login from './pages/Login'
 import './components/styles/reset.css';
+
 
 class App extends Component{
     render(){
-        return(
-            <LoginWrapper>
-                <LogoBotcamp />
-                <BtnBotcamp content="Entrar" /> 
-            </LoginWrapper >    
+        return( 
+            <Login />
         );
     }   
 }
---
+++ app/src/pages/Chat/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chat = () => (
+    <h1>Hello New page</h1>
+);
+
+export default Chat;
