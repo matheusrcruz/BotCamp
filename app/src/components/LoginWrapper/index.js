f4eb3b85b885224114cef4b11f00e32940a2909d
matheusrcruz <git.dev@outlook.com>
2019-04-19 14:31:28 -0300

    ADD: Posicionando os elementos

--- app/src/components/LoginWrapper/index.js
+++ app/src/components/LoginWrapper/index.js
@@ -2,10 +2,22 @@ import styled from 'styled-components';
 import background from '../img/ecossistema-background.png';
 
 const LoginWrapper = styled.main`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     background-image: url(${background});
+    box-sizing: border-box;
     width: 100vw;
     height: 100vh;
+    padding-top: 120px;
+    padding-bottom: 80px;
 `;
+//Display flex instancia flex
+// flex wrap: posicionar no centro
+//aling items: centralizar mas aonde?
+// justify content: no meio!
 export default LoginWrapper;
 
 
