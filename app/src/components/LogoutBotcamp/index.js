8a7a7c12fed21909610af28dfa23c63d568b40fb
matheusrcruz <git.dev@outlook.com>
2019-05-16 00:29:55 -0300

    Aula46

---
+++ app/src/components/LogoutBotcamp/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import logout from '../../components/img/logout.png';
+
+const LogoutBotcamp = styled.img.attrs({
+    src:logout ,
+    alt: "Incone para sair"
+})``;
+
+
+export default LogoutBotcamp;   
