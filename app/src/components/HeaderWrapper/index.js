18358ec137f5fdf36899a4854d50f2fb6fea06cc
matheusrcruz <git.dev@outlook.com>
2019-05-16 08:57:43 -0300

    Aula04 50

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const HeaderWrapper = styled.header`
     background-color: #26265e;
     padding: 10px 16px;
-    /* top/bottom  left/right*/
+    display: flex;
+    justify-content: space-between;
 `;
 
 export default HeaderWrapper;
